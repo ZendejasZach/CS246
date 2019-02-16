5e3328dea0df5acd9f3e11a02716028674c6d185
Zach Zendejas <ZAX1527@gmail.com>
2019-02-15 22:23:37 -0700

    Issue with progress bar. Trying to figure out.

--- Week06/TeamActivity/app/src/main/java/Zendejas/CS246/MainActivity.java
+++ Week06/TeamActivity/app/src/main/java/Zendejas/CS246/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -22,10 +24,10 @@ public class MainActivity extends AppCompatActivity {
     String filename = "numbers.txt";
     String sCurrentLine;
     FileOutputStream outputStream;
-    ProgressBar progressBar = findViewById(R.id.progressBar);
+//    ProgressBar progressBar = findViewById(R.id.progressBar2);
     int progressStatus = 0;
-    ArrayAdapter<String> numberAdapter = new ArrayAdapter<>(this, R.layout.activity_main, R.id.View_list, Collections.singletonList(sCurrentLine));
-    ListView listView = findViewById(R.id.View_list);
+//    ArrayAdapter<String> numberAdapter = new ArrayAdapter<>(this, R.layout.activity_main, R.id.View_list, Collections.singletonList(sCurrentLine));
+//    ListView listView = findViewById(R.id.View_list);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,24 +36,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Create button
-    public void createFile(View view){
+    public void createFile(View view) {
+        // Debugging
+        Toast create = Toast.makeText(this, "Create called", Toast.LENGTH_SHORT);
+        create.show();
+
         Button button = findViewById(R.id.button_Create);
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                try{
+                try {
                     // open fileStream
                     outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
 
                     // loop to add 1-10 and sleep on each line
                     //TODO: Test this to make sure the format is correct
-                    for (int i = 0; i < 10; i++){
+                    for (int i = 0; i < 10; i++) {
                         outputStream.write(Integer.parseInt(i + "\n"));
                         progressStatus++;
-                        progressBar.setProgress(progressStatus);
+//                        progressBar.setProgress(progressStatus);
                         Thread.sleep(250);
                     }
                     outputStream.close();
-                } catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -61,6 +67,10 @@ public class MainActivity extends AppCompatActivity {
     // Load button
     //TODO: Test this ish too
     public void loadFile(View view){
+        // Debugging
+        Toast load = Toast.makeText(this, "Load called", Toast.LENGTH_SHORT);
+        load.show();
+
         // prep
         Button button = findViewById(R.id.button_Load);
         progressStatus = 0;
@@ -82,10 +92,10 @@ public class MainActivity extends AppCompatActivity {
 
                         // update the progress bar
                         progressStatus++;
-                        progressBar.setProgress(progressStatus);
+//                        progressBar.setProgress(progressStatus);
 
                         // add to list
-                        listView.setAdapter(numberAdapter);
+//                        listView.setAdapter(numberAdapter);
 
                         // sleepy time
                         Thread.sleep(250);
@@ -105,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 // clear the adapter
-                numberAdapter.clear();
+//                numberAdapter.clear();
             }
         });
     }
